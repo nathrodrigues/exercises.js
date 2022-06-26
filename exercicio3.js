78bd0bf27d571efc654b4127e1362f068cc40d4e
nathrodrigues <c.nathaliarodrigues@gmail.com>
2022-06-25 23:15:07 -0300

    exercise 2 add

--- exercicio3.js
+++ exercicio3.js
@@ -10,3 +10,22 @@ function verificaPalindromo(string) {
 }
 
 console.log(verificaPalindromo("ama"))
+
+
+/**
+ * Solução 2
+ */
+
+function verificaPalindromo2(string) {
+    if(!string) return "string inexistente"
+
+    for(let i = 0; i < string.length / 2; i++) {
+        if(string[i] !== string[string.length -1 -i]) {
+            return false
+        }
+    }
+
+    return true
+}
+
+console.log(verificaPalindromo2('raiar'))
