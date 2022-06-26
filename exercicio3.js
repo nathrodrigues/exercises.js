859619aa90038e8df0059e0d9c8a33338f10242f
nathrodrigues <c.nathaliarodrigues@gmail.com>
2022-06-25 22:41:15 -0300

    first commit

---
+++ exercicio3.js
@@ -0,0 +1,12 @@
+/**
+* Verifique, de duas maneiras diferentes entre si, se uma String é um palíndromo.
+* Palíndromo: frase ou palavra que se pode ler, indiferentemente, da esquerda para a direita ou vice-versa (ex.: raiar, ama, ovo, radar)
+ */
+
+function verificaPalindromo(string) {
+    if(!string) return
+
+    return string.split("").reverse().join("") === string
+}
+
+console.log(verificaPalindromo("armario"))
